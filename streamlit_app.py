1bfdb7fd1343321830406eb1606b9109eb3c74be
asalzwed <salzweda@gmail.com>
2025-02-17 00:33:49 +0000

    gauges

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import numpy as np
 from typing import List, Tuple
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import plotly.graph_objects as go
 
 cont_pitch_measures = [
         "release_speed", "release_pos_x", "release_pos_y", "release_pos_z","balls",
@@ -16,9 +17,16 @@ cont_pitch_measures = [
 
 cat_pitch_measures = ["pitch_type","zone","stand","p_throws","type"]
 
-# Show the page title and description.
-st.set_page_config(page_title="The Long Ball", page_icon="⚾",layout="wide",
-        initial_sidebar_state="expanded")
+st.markdown("""
+    <style>
+        .stMultiSelect [data-baseweb=select] span{
+            max-width: 500px;
+            font-size: 0.6rem;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+
 st.sidebar.header("Filters")
 st.title("⚾ The Long Ball")
 st.write(
@@ -46,7 +54,7 @@ def load_data():
 
     # Create barrel column
     df['barreled'] = df.apply(lambda row: is_barrel(row['launch_speed'], row['launch_angle']), axis=1)
-    df['game_date'] = pd.to_datetime(df["game_date"],format='mixed')
+    
     return df
 
 def encoder_scaler(df: pd.DataFrame):
@@ -61,7 +69,7 @@ def encoder_scaler(df: pd.DataFrame):
     
     return encoder, scaler
 
-def compute_pitch_attributes(df: pd.DataFrame, encoder: OneHotEncoder, scaler: StandardScaler, ids: List, subset: List, ptype='pitcher'):
+def compute_pitch_attributes(df: pd.DataFrame, encoder: OneHotEncoder, scaler: StandardScaler, ids: List, subset: List, ptype='pitcher_name_id'):
     """
     In progress...
     """
@@ -88,6 +96,22 @@ def compute_pitch_attributes(df: pd.DataFrame, encoder: OneHotEncoder, scaler: S
    
     return raw_profiles.round(2), stat_profiles.round(2)
 
+def compute_metric(df: pd.DataFrame, field: str, event, delta: int, date_flag=0):
+        total_events = len(df)
+        num_events = len(df[df[field]==event])
+
+        if date_flag==0:
+            last_game = df['game_date'].max()
+        else: 
+            last_game = pd.Timestamp.today()   
+
+        df_dX = df[(df['game_date']<=last_game) & (df['game_date']>=(last_game-pd.Timedelta(days=delta)))]
+
+        dX_total_events = len(df_dX)
+        dX_num_events = len(df_dX[df_dX[field]==event])
+
+        return [[num_events,total_events],[dX_num_events,dX_total_events]], last_game
+
 def player_card(name, ban):
     """Reusable Player Card Component"""
     
@@ -104,59 +128,70 @@ df = load_data()
 encoder, scaler = encoder_scaler(df)
 try: 
 
-    pitcher = st.sidebar.selectbox(
+    pitcher = st.sidebar.multiselect(
         "Pitcher",
-        df.pitcher.unique(),
+        df.pitcher_name_id.unique(),
         placeholder="Select a pitcher to analyze...",
+        default = 'Paul Skenes (694973)',
+        max_selections=1,
         help=None
     )
+
     batter = st.sidebar.multiselect(
         "Batter",
-        df.batter.unique(),
+        df.batter_name_id.unique(),
         placeholder="Select a batter to analyze...",
-        help=None,
-        default=[665161]
+        default=['Aaron Judge (592450)','Juan Soto (665742)'],
+        help=None
     )
 
-    pitcher_att, pitcher_es = compute_pitch_attributes(df, encoder, scaler, [pitcher], subset=[])
-    batter_att, batter_es = compute_pitch_attributes(df, encoder, scaler, batter,['barreled',1],'batter')
+    pitcher_att, pitcher_es = compute_pitch_attributes(df, encoder, scaler, pitcher, subset=[])
+    batter_att, batter_es = compute_pitch_attributes(df, encoder, scaler, batter,['barreled',1],'batter_name_id')
 
-    cos_sim_score = cosine_similarity(pitcher_es, batter_es)#[0][0]
+    cos_sim_score = cosine_similarity(pitcher_es, batter_es)
 
     cols = st.columns(len(batter)+1)
     with cols[0]:
-        player_card(pitcher,0)
+        player_card(pitcher[0],0)
    
     for i,x in enumerate(batter):
         with cols[i+1]:
             player_card(x,cos_sim_score[0][i])
     
-    def compute_metric(df: pd.DataFrame, field: str, event):
-        total_events = len(df)
-        num_events = len(df[df[field]==event])
-
-        last_game = df['game_date'].max()
-
-        df_dX = df[(df['game_date']<=last_game) & (df['game_date']>=(last_game-pd.Timedelta(days=30)))]
-
-        dX_total_events = len(df_dX)
-        dX_num_events = len(df_dX[df_dX[field]==event])
-
-        return num_events/total_events, dX_num_events/dX_total_events, last_game
-
-
-    ptype = pitcher_es.filter(like="pitch_type_")
-    ptype.columns = [col.replace("pitch_type_", "") for col in ptype.columns]
+    
+    #ptype = pitcher_es.filter(like="pitch_type_")
+    #ptype.columns = [col.replace("pitch_type_", "") for col in ptype.columns]
     #st.bar_chart(ptype[ptype>0].iloc[0,:])
     #st.bar_chart(ptype.iloc[0,:],horizontal=True,width=100, use_container_width=False)
+   
+    metrics, lgame = compute_metric(df[df['pitcher_name_id']==pitcher[0]],'barreled',1,delta=30)
+    
+    def metric_rate_card(name,metrics,factor=85):
+        baseline_rate = (metrics[0][0]/metrics[0][1])*factor*100
+        latest_rate = (metrics[1][0]/metrics[1][1])*factor*100
+        change_rate = (latest_rate-baseline_rate)/baseline_rate * 100
+        st.metric(name,value =str(round(latest_rate,1)) + "%",delta=str(round(change_rate,1)) + "%")
+    
+    metric_rate_card("Barrel Rate", metrics)
 
-    a,b,c = compute_metric(df[df['pitcher']==pitcher],'barreled',1)
-    b_delta = ((b*30)-(a*30))/(a*30)*100
-    st.metric('Barrelled Ball Rate',value =str(round(a*30*100,1)) + "%",delta=str(round(b_delta,1)) + "%")
-    a,b,c = compute_metric(df[df['batter']==batter[0]],'barreled',1)
-    b_delta = ((b*30)-(a*30))/(a*30)*100
-    st.metric('Barreled Ball Rate',value =str(round(a*30*100,1)) + "%",delta=str(round(b_delta,1)) + "%")
- 
+    metrics2, lgame = compute_metric(df,'barreled',1,delta=30)
+    
+    fig = go.Figure(go.Indicator(
+    mode = "gauge+number+delta",
+    value =  (metrics[1][0]/metrics[1][1])*100,
+    number_font_color="black",
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    title = {'text': "Barrel Rate (per 100)", 'font': {'size': 24,'color':'black'}},
+    delta = {'reference': (metrics[0][0]/metrics[0][1])*100,'increasing': {'color': "green"},'decreasing': {'color':'red'}},
+    gauge = {
+        'axis': {'range': [None, 5], 'tickwidth': 1},
+        'bar': {'color': "rgba(0,0,0,0)", "thickness": 1},
+        'bgcolor': "white",
+        'borderwidth': 2,
+        'steps' : [{'range': [0, (metrics2[0][0]/metrics2[0][1])*100], 'color': "lightgray"},
+                    {'range': [(metrics[1][0]/metrics[1][1])*100, (metrics[0][0]/metrics[0][1])*100], 'color': "red" if (metrics[1][0]/metrics[1][1])<(metrics[0][0]/metrics[0][1]) else "green","thickness":0.5}],
+        'threshold' : {'line': {'color': "black", 'width': 4}, 'thickness': 0.75, 'value': (metrics[1][0]/metrics[1][1])*100}}))
+    fig
  
 except:
     st.write("Please select a pitcher and batter with data...")
